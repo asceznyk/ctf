bbc16b5ae2cbbd8c95ab0254ef8250ffd26fe9fb
asceznyk <asceznyk@gmail.com>
2023-09-20 13:02:57 +0530

    removed unnecessary files

--- picoctf/xdec.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-enc = sys.argv[1]
-bins = [bin(ord(c)).replace('0b','') for c in enc]
-bins = ['0'+b if len(b) < 15 else b for b in bins]
-print(bins, [len(b) for b in bins])
-print(''.join([chr(int(b[:7],2))+chr(int(b[8:],2)) for b in bins]))
-
